04c7d38ba20a5af18a5f0cbd542b89573dc351be
Anna Damm <793921+AnnaDamm@users.noreply.github.com>
2020-02-23 20:29:51 +0100

    only show footer on message home view to not overlap with other elements on the page

--- webview.js
+++ webview.js
@@ -5,15 +5,23 @@ module.exports = Franz => {
 
     // waiting for Javascript to be loaded
     document.addEventListener('DOMContentLoaded', () => {
+
         // waiting for AngularJS to be fully loaded
         angular.element(() => {
             const statusController = angular.element(document.querySelector('html')).controller();
-            const stateService = statusController.webClientService.stateService;
+            const webClientService = statusController.webClientService;
+            const stateService = webClientService.stateService;
 
             // listen to changes in unreadCount
             stateService.evtUnreadCountChange.attach(count => {
                 Franz.setBadge(count)
             });
+
+            // show/hide info text at the top only when in messenger view
+            const bodyElement = document.querySelector('body');
+            const $transitions = angular.element(bodyElement).injector().get('$transitions');
+            $transitions.onStart({to: 'messenger.home'}, () => bodyElement.classList.add('franz-messenger-home'));
+            $transitions.onStart({from: 'messenger.home'}, () => bodyElement.classList.remove('franz-messenger-home'));
         });
     });
 };
